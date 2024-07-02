def7c89e21259f0261db8b012cd8324fac7899ee
Rémi THOMAS <remi@xtware.com>
2024-07-02 12:42:08 +0200

    First version

---
+++ demo/Program.cs
@@ -0,0 +1,5 @@
+﻿FileStream fileStream = new FileStream("demo.eml", FileMode.Open, FileAccess.Read);
+FileStream fout = new FileStream("demo.html", FileMode.Create, FileAccess.Write);
+eml2html.Convert(fileStream, fout);
+fileStream.Close();
+fout.Close();
---
+++ eml2html/eml2html.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.IO;
+using MimeKit;
+
+public class eml2html
+{
+    static string _formatHtmlTag(string src)
+    {
+        src = src.Replace(">", "&gt;");
+        src = src.Replace("<", "&lt;");
+        return src;
+    }
+
+    static string _formatAddresses(InternetAddressList addresses, string prefix)
+    {
+        StringBuilder buffer = new StringBuilder();
+        buffer.Append(string.Format("<b>{0}:</b> ", prefix));
+
+        string sep = "";
+        foreach (MailboxAddress address in addresses)
+        {
+            buffer.Append(sep + _formatHtmlTag(address.ToString()));
+            sep = ";";
+        }
+
+        buffer.Append("<br>");
+        return buffer.ToString();
+    }
+
+    static public void Convert(Stream eml, Stream htmlOut)
+    {
+        MimeMessage mail = MimeMessage.Load(eml);
+
+        string html = mail.HtmlBody;
+        StringBuilder header = new StringBuilder();
+
+        header.Append("<font face=\"Courier New,Arial\" size=2>");
+        header.Append("<b>From:</b> " + _formatHtmlTag(mail.From.ToString()) + "<br>");
+
+        header.Append(_formatAddresses(mail.To, "To"));
+        header.Append(_formatAddresses(mail.Cc, "Cc"));
+        header.Append("<b>Date:</b>" + mail.Date.ToString("yyyy-MM-dd HH:mm:ss") + "<br>");
+
+        header.Append(string.Format("<b>Subject:</b>{0}<br>\r\n", _formatHtmlTag(mail.Subject)));
+
+        // Change original meta header encoding to utf-8
+        Regex reg = new Regex("(<meta[^>]*charset[ \t]*=[ \t\"]*)([^<> \r\n\"]*)", RegexOptions.Multiline | RegexOptions.IgnoreCase);
+        html = reg.Replace(html, "$1utf-8");
+        if (!reg.IsMatch(html))
+        {
+            header.Insert(0, "<meta HTTP-EQUIV=\"Content-Type\" Content=\"text/html; charset=utf-8\">");
+        }
+
+        html = header.ToString() + "<hr>" + html;
+        StreamWriter writer = new StreamWriter(htmlOut, Encoding.UTF8);
+        writer.Write(html);
+        writer.Flush();
+    }
+}
